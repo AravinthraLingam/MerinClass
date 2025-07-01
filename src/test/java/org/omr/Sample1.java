eff6192d07071eaeb78ad908817d38144b65e734
Arainthralingam <aravinthralingam@gmail.com>
2025-07-01 11:14:26 +0530

    First time push

---
+++ src/test/java/org/omr/Sample1.java
@@ -0,0 +1,14 @@
+package org.omr;
+
+public class Sample1 {
+	private void csk() {
+		System.out.println("Chennai");
+
+	}
+	private void mi() {
+		System.out.println("Mumbai");
+	}
+	private void rcb() {
+		System.out.println("Banglore");
+	}
+}
